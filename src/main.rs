655e2e0823de9a28be6a2402b4dade1b20b5b1ae
Aaron Kofsky <aaronko@umich.edu>
2018-10-12 20:55:25 -0400

    Make skeleton struct for Grid

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
+use ggez::graphics::{DrawMode, Point2, Rect, Vector2};
 use ggez::{graphics, timer, Context, GameResult};
 use std::path;
 
-struct MainState;
+struct MainState {
+    grid: Grid,
+}
 
 impl MainState {
     fn new(_ctx: &mut Context) -> GameResult<MainState> {
-        Ok(MainState)
+        Ok(MainState {
+            grid: Default::default(),
+        })
     }
 }
 
@@ -16,18 +21,64 @@ impl ggez::event::EventHandler for MainState {
         while timer::check_update_time(ctx, DESIRED_FPS) {
             // fixed update
         }
+        self.grid.world_offset.0 += 0.01;
+        self.grid.world_offset.1 += 0.01;
         Ok(())
     }
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
         graphics::clear(ctx);
 
-        // draw ???
+        self.grid.draw(ctx)?;
 
         graphics::present(ctx);
         Ok(())
     }
 }
+
+type GridCoord = usize;
+type WorldCoord = f32;
+
+const GRID_WIDTH: GridCoord = 32;
+const GRID_HEIGHT: GridCoord = 8;
+const TILE_SIZE: WorldCoord = 1.0f32;
+/// A grid contains the collidable tiles that our dynamic objects interact with
+///
+struct Grid {
+    grid: [[Tile; GRID_WIDTH]; GRID_HEIGHT],
+    world_offset: (WorldCoord, WorldCoord),
+}
+
+impl Grid {
+    fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
+        for (j, row) in self.grid.iter().enumerate() {
+            for (i, _tile) in row.iter().enumerate() {
+                let x_pos = self.world_offset.0 + TILE_SIZE * i as f32;
+                let y_pos = self.world_offset.1 + TILE_SIZE * j as f32;
+                let rect = Rect {
+                    x: x_pos,
+                    y: y_pos,
+                    w: TILE_SIZE,
+                    h: TILE_SIZE,
+                };
+                graphics::rectangle(ctx, graphics::DrawMode::Fill, rect)?;
+            }
+        }
+        Ok(())
+    }
+}
+
+impl Default for Grid {
+    fn default() -> Grid {
+        Grid {
+            grid: [[Tile {}; GRID_WIDTH]; GRID_HEIGHT],
+            world_offset: (0.0, 12.0),
+        }
+    }
+}
+
+#[derive(Copy, Clone)]
+struct Tile {}
 
 fn main() {
     let c = ggez::conf::Conf::new();
